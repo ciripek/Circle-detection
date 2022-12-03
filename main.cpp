95ef27884ea024cbc6aff62ddccacfdfc1635173
Varga Bence <wben0802@gmail.com>
2022-12-03 18:59:45 +0100

    reading points from a text file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
-#include <fmt/core.h>
+#include <vector>
+#include <fstream>
+#include <iterator>
+#include <fmt/ranges.h>
 
-int main(){
-   fmt::print("Hello, World");
+#include "Point.hpp"
+
+void read_from_file(const char* filename, std::vector<Point>& points);
+
+
+int main(int argc , char** argv) {
+    std::vector<Point> points;
+    if (argc != 2) {
+        fmt::print(stderr, "Usage: cirle_detection file.text");
+    }
+
+
+    read_from_file(argv[1], points);
+
+    if (points.empty()) {
+        fmt::print(stderr, "Reading from the file was");
+    }
+
+    fmt::print("{}", points);
+    return 0;
+}
+
+void read_from_file(const char* filename,std::vector<Point>& points){
+    std::ifstream inputstream(filename);
+    points.assign(std::istream_iterator<Point>(inputstream), std::istream_iterator<Point>());
 }
