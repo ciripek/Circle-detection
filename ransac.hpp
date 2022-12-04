26909d7992bf9749d30e7ca8d8d27e9b41314f7c
Varga Bence <wben0802@gmail.com>
2022-12-04 13:02:53 +0100

    main ransac algorithm

--- ransac.hpp
+++ ransac.hpp
@@ -2,14 +2,41 @@
 #define CIRCLE_DETECTION_RANSAC_HPP
 
 #include "Circle.hpp"
+#include <vector>
+#include <array>
+#include <random>
+#include <algorithm>
 
 class RANSAC {
 public:
     RANSAC(int iteration, float error): iteration(iteration), error(error){}
+
+    void run(const std::vector<Point>& dataSet){
+        for (int i = 0; i < iteration; ++i) {
+            std::array<Point, sample_size> point;
+            std::sample(dataSet.cbegin(), dataSet.cend(), point.begin(), sample_size, gen);
+
+            Circle circle = Circle::CircleFromThreePoints(point);
+            count_supporting_points(circle, dataSet, error);
+
+            if (circle.getSupportedPoints() > bestModel.getSupportedPoints()){
+                bestModel = circle;
+            }
+        }
+    }
 private:
     int iteration;
     float error;
     Circle bestModel;
+    std::mt19937 gen{std::random_device{}()};
+    static constexpr std::size_t sample_size = 3U;
+    
+    static void count_supporting_points(Circle& circle, const std::vector<Point>& dataSet, float error) {
+        int count = std::count_if(dataSet.cbegin(), dataSet.cend(), [error, &circle](const Point& point){
+            return circle.is_point_supported(point, error);
+        });
+        circle.setSupportedPoints(count);
+    }
 };
 
 #endif //CIRCLE_DETECTION_RANSAC_HPP
